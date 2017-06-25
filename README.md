8e1a4bb6356fdf70eaabcc2111e95b84afa0558c
theWhiteFox <ste@thewhitefox.ninja>
2017-06-25 13:07:11 +0100

    :cherry_blossom: display fibonacci gist and output HTML :card_index:

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Learning to advance my skills in programming though algorithms
 ## Fibonacci
 ![](https://cdn-images-1.medium.com/max/2000/1*2laBY0J-h42yqCBm9FQmIQ.jpeg)
 
+[Fibonacci sequence algorithm in Javascript](https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e)
 
 
+[https://rawgit.com/](rawgit)
--- scripty.js
+++ scripty.js
@@ -1,16 +1,20 @@
+
 function fibonacci() {
 	var first = 1,
     	second = 0,
         answer = first + second;
 		console.log(answer);
+
+        var text = "";
        
     while(answer < 100) {
     	console.log(answer);
-	document.write('<br>' + answer);
+        text += "<br>" + answer;	    
         second = first;
         first = answer;
         answer = first + second;
     }
+    return text;
 }
 
-fibonacci(); // prints to both console and page
+document.getElementById("fibWhile").innerHTML = fibonacci();
