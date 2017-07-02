154cd2ac3da08a78705dcbfc6480176d46852340
theWhiteFox <ste@thewhitefox.ninja>
2017-07-02 11:14:27 +0100

    :milky_way: while loops :dolls:

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Algorithms
 Learning to advance my skills in programming though algorithms
 
+Fibonacci functions created using 
+- while loops
+- 
+
 ## Fibonacci
 ![](https://cdn-images-1.medium.com/max/2000/1*2laBY0J-h42yqCBm9FQmIQ.jpeg)
 
 [Fibonacci sequence algorithm in Javascript](https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e)
 
+[c fibonacci-problem-using-while-loop](https://cboard.cprogramming.com/c-programming/57359-fibonacci-problem-using-while-loop.html)
 
-[https://rawgit.com/](rawgit)
+
+
+[rawgit](https://rawgit.com/)
+[about gists](https://help.github.com/articles/about-gists/)
---
+++ src/scripty.js
@@ -0,0 +1,65 @@
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+// Fn = Fn-1 + Fn-2
+// F0 = 0, F1 = 1
+
+
+// fibonacci html output
+function fibonacciHTML() {
+    var result = 0, term1 = 0, term2 = 1, i = 1;
+
+    var text = term1 + '<br>' + term2;
+
+    while(i < 10) {
+        result = term1 + term2;
+        text += "<br>" + result;
+        term1 = term2;
+        term2 = result;
+        i++;
+    }
+    return text;
+}
+
+// error in console document.getElementById(...) is null
+window.onload = function() {
+    document.getElementById("fibWhile").innerHTML = basicFib();
+}
+
+// very basic fibonacci
+function basicFib() {
+	var first = 1,
+    	second = 0,
+        answer = first + second;   
+       
+    while(answer < 100) {    	
+        console.log(answer);	    
+        second = first;
+        first = answer;
+        answer = first + second;
+    }
+    return 0;
+}
+
+function fibonacci(num) {
+    var a = 1, b = 0, temp2;
+
+    while(num >  0) {
+        temp2 = a;
+        a = a + b;
+        b = temp2;
+        num--;
+        console.log(temp2);
+    }
+    return b;
+}
+
+// not using while loop
+
+function fib(number) {
+
+    if(number == 0) return 0;
+    
+    if(number == 1) return 1;
+
+    return fib(number - 2) + fib(number - 1);
+
+}
