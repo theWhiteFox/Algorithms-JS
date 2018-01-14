5cbe332d00ed4b627669349f71eefcd93cbe63fe
Ó Conchubhair <ste@thewhitefox.ninja>
2018-01-14 11:39:55 +0000

    :cake: FizzBuzz :coffee:

--- src/scripty.js
+++ src/scripty.js
@@ -12,12 +12,12 @@
 // below functions can be accessed from the console
 // very basic fibonacci
 function basicFib() {
-	var first = 1,
-    	second = 0,
-        answer = first + second;   
-       
-    while(answer < 100) {    	
-        console.log(answer);	    
+    var first = 1,
+        second = 0,
+        answer = first + second;
+
+    while (answer < 100) {
+        console.log(answer);
         second = first;
         first = answer;
         answer = first + second;
@@ -29,7 +29,7 @@ function basicFib() {
 function fibonacci(num) {
     var a = 1, b = 0, temp2;
 
-    while(num >  0) {
+    while (num > 0) {
         temp2 = a;
         a = a + b;
         b = temp2;
@@ -44,7 +44,7 @@ function fibFor() {
     var a = 0, b = 1, i = 1, result;
     result = b;
     console.log(a + '\n' + result + '\n');
-    for(i; i < 10; i++) {
+    for (i; i < 10; i++) {
         console.log(result + '\n');
         result = a + b;
         a = b;
@@ -55,9 +55,9 @@ function fibFor() {
 // recursive function starts at 0
 function fib(number) {
 
-    if(number == 0) return 0;
-    
-    if(number == 1) return 1;
+    if (number == 0) return 0;
+
+    if (number == 1) return 1;
 
     return fib(number - 2) + fib(number - 1);
 
@@ -65,7 +65,7 @@ function fib(number) {
 
 // shorter recursive function starts at 1
 function fibRecursive(n) {
-    if(n <= 1) return 1;
+    if (n <= 1) return 1;
 
     return fibRecursive(n - 2) + fibRecursive(n - 1);
 }
@@ -79,7 +79,12 @@ function fibRecursive(n) {
 console.log("modulus operator: 7 % 3 = " + 7 % 3);
 
 function fizzBuzz(num) {
-
+    for (var i = 1; i <= num; i++) {
+        if (i % 15 === 0) console.log("FizzBuzz");
+        else if (i % 5 === 0) console.log("fizz");
+        else if (i % 3 === 0) console.log("Buzz");
+        else console.log(i);
+    }
 }
 
 fizzBuzz(20);
