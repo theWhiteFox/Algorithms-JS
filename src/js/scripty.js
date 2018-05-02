a983d19f068cbc0cb0530df874038178eea12894
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-02 14:14:11 +0100

    :v: Mean Median & Mode :a:

--- src/js/scripty.js
+++ src/js/scripty.js
@@ -366,13 +366,12 @@
   }
   console.log(reverseString1("This is a test1"));
 
-  // reverse array
+  // Reverse Array
   function reverseArrInPlace(arr) {
     for (let i = 0; i < arr.length / 2; i++) {
-      // reverse array
       let temp = arr[i];
-      arr[i] = arr[arr.length -1 - i];
-      arr[arr.length -1 - i] = temp;
+      arr[i] = arr[arr.length - 1 - i];
+      arr[arr.length - 1 - i] = temp;
     }
     // return reversed arr
     return arr;
@@ -383,5 +382,68 @@
   console.log(`Array ${arr}`);
   console.log(`Reverse arr loop: ${reverseArrInPlace(arr)}`);
   console.log(`Using the method: ` + arr.reverse());
-  
+
+  function getMean(array) {
+    let sum = 0;
+
+    array.forEach(num => {
+      sum += num;
+    });
+
+    let mean = sum / array.length;
+    return mean;
+  }
+
+  function getMedian(array) {
+    array.sort((a, b) => {
+      return a - b;
+    });
+    let median;
+
+    if (array.length % 2 !== 0) {
+      median = array[Math.floor(array.length / 2)];
+    } else {
+      let mid1 = array[array.length / 2 - 1];
+      let mid2 = array[array.length / 2];
+      median = (mid1 + mid2) / 2;
+    }
+    return median;
+  }
+
+  function getMode(array) {
+    let modeObject = {};
+
+    array.forEach(num => {
+      if (!modeObject[num]) {
+        modeObject[num] = 0;
+      }
+      modeObject[num]++;
+    });
+
+    let maxFrequency = 0;
+    let modes = [];
+    for (let num in modeObject) {
+      if (modeObject[num] > maxFrequency) {
+        modes = [num];
+        maxFrequency = modeObject[num];
+      } else if (modeObject[num] === maxFrequency) {
+        modes.push(num);
+      }
+    }
+    if (modes.length === Object.keys(modeObject).length) {
+      modes = [];
+    }
+    return modes;
+  }
+
+  function meanMedianMode(array) {
+    // call other 3 funcs
+    // return obj which has mean, median, mode in it
+    return {
+      mean: getMean(array),
+      median: getMedian(array),
+      mode: getMode(array)
+    };
+  }
+  console.log(meanMedianMode([1, 2, 3, 4, 5, 4, 6, 1]));
 })(); // end IIFE
