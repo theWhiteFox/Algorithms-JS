57e6b916af009a141c4008e36d796b72dd60b046
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-23 20:56:48 +0100

    :fu: split it up :coffee:

---
+++ two-sum/twosum.js
@@ -0,0 +1,14 @@
+// Two Sum return indices O(n^2)
+const twoSumBrute = (arr, target) => {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] == target - arr[i]) {
+        return [i, j];
+      }
+    }
+  }
+  return "Error 2 numbers for: " + target;
+};
+
+const arr = [3, 7, 11, 15];
