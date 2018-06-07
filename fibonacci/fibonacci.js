6739ced1cb6e359211a3e409027938d06f491450
theWhiteFox <ste@thewhitefox.ninja>
2018-06-07 21:43:52 +0100

    :v: fibonacci :lemon:

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,18 +1,32 @@
 (function() {
-    // 1, 1, 2, 3, 5, 8, 11
-    function fibonacci(num) {
-        var a = 1,
-          b = 0,
-          temp2;
-    
-        while (num > 0) {
-          temp2 = a;
-          a = a + b;
-          b = temp2;
-          num--;
-          console.log(temp2);
-        }
-        return b;
+  // // 1, 1, 2, 3, 5, 8, 11
+  // function fibonacci(num) {
+  //   var a = 1,
+  //     b = 0,
+  //     temp2;
+
+  //   while (num > 0) {
+  //     temp2 = a;
+  //     a = a + b;
+  //     b = temp2;
+  //     num--;
+  //     console.log(temp2);
+  //   }
+  //   return b;
+  // }
+  // fibonacci(7);
+
+  const basicFib = () => {
+    let first = 1,
+      second = 0,
+      answer = first + second;
+
+    while (answer < 10) {
+      console.log(answer);
+      second = first;
+      first = answer;
+      answer = first + second;
     }
-    fibonacci(7);
-}());
+    basicFib();
+  };
+})();
